60bfbfa538b1d64b5953d1dc0aebade64f43e90e
nick0726 <nick0726@naver.com>
2022-09-15 17:04:23 +0900

    Update DropDown test

--- DropDown/src/components/Nav.js
+++ DropDown/src/components/Nav.js
@@ -5,27 +5,54 @@ import MobileMenuIcon from "./MobileMenuIcon";
 const Nav = () => {
   const mobileMenuRef = useRef();
   const menuIconRef = useRef();
+  const menuFoldRef = useRef();
   const handleClick = (e) => {
     mobileMenuRef.current.classList.toggle("mobileMenu-active");
     menuIconRef.current.classList.toggle("menuIcon-active");
+    menuFoldRef.current.classList.toggle("menuFoldRef-active");
+  };
+  const menuFold = (e) => {
+    menuFoldRef.current.classList.toggle("menuFoldRef-active");
   };
   return (
-    <div id='Nav'>
-      <div className='nav-container'>
-        <div className='nav-title'>TITLE</div>
-        <div
-          className='nav-menu'
-          ref={menuIconRef}
-          onClick={(e) => handleClick(e)}
-        >
-          <MobileMenuIcon />
+    <>
+      <div id='Nav'>
+        <div className='nav-container'>
+          <div className='nav-title'>TITLE</div>
+          <div
+            className='nav-menu'
+            ref={menuIconRef}
+            onClick={(e) => handleClick(e)}
+          >
+            <div id='MobileMenuIcon'>
+              <span className='bar'></span>
+              <span className='bar'></span>
+              <span className='bar'></span>
+            </div>
+          </div>
+        </div>
+
+        <div className='nav-mobileMenu' ref={mobileMenuRef}>
+          <div id='MobileMenu'>
+            <div
+              onClick={(e) => menuFold(e)}
+              // onClick={(e) => handleClick(e)}
+            >
+              LOGIN
+              <div className='nav-menuFoldRef' ref={menuFoldRef}>
+                <div>Sub1</div>
+                <div>Sub2</div>
+                <div>Sub3</div>
+                <div>Sub4</div>
+                <div>Sub5</div>
+              </div>
+            </div>
+            <div>SIGNUP</div>
+            <div>ABOUT US</div>
+          </div>
         </div>
       </div>
-
-      <div className='nav-mobileMenu' ref={mobileMenuRef}>
-        <MobileMenu />
-      </div>
-    </div>
+    </>
   );
 };
 
